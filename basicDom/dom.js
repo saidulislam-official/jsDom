7efb7d8bf913d681b8fcc117b26c539f660151b4
saidulislam-official <saidulislam.own@gmail.com>
2025-04-07 18:31:10 +0600

    first commit

---
+++ basicDom/dom.js
@@ -0,0 +1,17 @@
+const allLi = document.getElementsByTagName('li');
+// console.log(allLi);
+
+const aboutMe = document.getElementById('aboutMe');
+// console.log(aboutMe);
+// console.log(aboutMe.innerText);
+
+
+const myProjectGroup = document.getElementsByClassName('myProjectGroups');
+// console.log(myProjectGroup);
+
+for(const li of myProjectGroup){
+    // console.log(li.innerText);
+}
+
+// const aboutMeBG = document.getElementById('aboutMe').style.backgroundColor = 'yellow';
+// console.log(aboutMeBG);
---
+++ basicDom/styleInJs.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    // console.log(section);
+    section.style.border = '2px solid lightblue';
+    section.style.margin = '20px';
+    section.style.padding = '20px';
+    section.style.backgroundColor = 'LightGray';
+
+
+
+}
